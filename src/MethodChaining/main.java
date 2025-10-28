1cd7d77cf708c64661b844c54ffa863023aae6d4
SchwartzLucas <lucassdesouza10@gmail.com>
2025-10-27 23:02:32 -0300

    Novo jeito de fazer usando classe (muito melhor) e mais didático muito bom fazer

--- src/MethodChaining/main.java
+++ src/MethodChaining/main.java
@@ -8,6 +8,12 @@ public class main {
         name = (name.concat(" Schwartz").toUpperCase());
 
         System.out.println(name);
+
+        Presenting p1 = new Presenting();
+
+        p1.setName("Lucas").setAge(21).setGender('M');
+
+        p1.Presenting();
     }
 
 }
