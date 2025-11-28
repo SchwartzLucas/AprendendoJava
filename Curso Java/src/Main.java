5ba78081e5e45900a19703e1c96fe80e68d46bab
SchwartzLucas <lucassdesouza10@gmail.com>
2025-11-28 18:04:55 -0300

    Subindo arquivos

---
+++ Curso Java/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("/Users/lucasschwartzdesouza/Documents/teste.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }finally {
+            if (sc == null) {
+                sc.close();
+            }
+        }
+    }
+    }
